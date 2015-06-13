f20b49335be2e9bf99b0254ced7089b43c6e01e5
Aruna Tebel <arunatebel@hotmail.com>
2015-06-13 16:00:59 +0530

    Created helper class - DbHelper

--- index.php
+++ index.php
@@ -5,3 +5,21 @@
  * Date: 6/13/2015
  * Time: 3:47 PM
  */
+?>
+
+<html>
+    <head>
+        <title>FYP Sandbox</title>
+    </head>
+    <body>
+        <form action="index.php">
+            First name:<br>
+            <input type="text" name="firstname" value="Mickey">
+            <br>
+            Last name:<br>
+            <input type="text" name="lastname" value="Mouse">
+            <br><br>
+            <input type="submit" value="Submit">
+        </form>
+    </body>
+</html>
